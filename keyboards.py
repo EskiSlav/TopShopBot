bae671018e4269b00e36df0d79e23df11fb78f2a
EskiSlav <uberbug544@gmail.com>
2020-01-23 11:45:43 +0200

    Added option to call inline language keybord and create inline keyboard from list of tuples

--- keyboards.py
+++ keyboards.py
@@ -16,25 +16,39 @@ class ReplyKeyboard(Keyboard):
     @classmethod
     def hello(cls):
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        keyboard.add("FAQ", "Каталог Товаров")
+        keyboard.add()
         return keyboard
+
+    @classmethod
+    def generate_language_keyboard(cls):
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add('EN', 'RU', 'UA')
 
 
 
 class InlineKeyboard(Keyboard):
 
     @classmethod
-    def generate_keyboard(cls, *args, **kwargs):
-        keyboard = types.InlineKeyboardMarkup()
-        for i in args:
-            keyboard.add(types.InlineKeyboardButton(text=i, callback_data=i + "_Callback"))
-        return keyboard
-
-    @classmethod
     def hello(cls):
         cls.keyboard.add(types.InlineKeyboardButton("FAQ"),types.InlineKeyboardButton("Каталог Каналов"))
 
+    @classmethod
+    def create_keyboard(cls, buttons:list, *args, **kwargs):
+        keyboard = types.InlineKeyboardMarkup()
+        buttons_ready = []
+        for button in buttons:
+            buttons_ready.append(types.InlineKeyboardButton(button[0], callback_data='lang ' +button[1]))
+        keyboard.row(buttons_ready[0], buttons_ready[1], buttons_ready[2])
+        return keyboard
 
+    @classmethod
+    def create_language_keyboard(cls):
+        lst = [
+                ('EN', 'en'),
+                ('RU', 'ru'),
+                ('UA', 'ua')
+            ]
+        return InlineKeyboard.create_keyboard(lst)
 
 
 # class Test:
