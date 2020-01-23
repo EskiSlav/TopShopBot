0942e9b413386fa759dee0487e903cbead9c95ab
EskiSlav <uberbug544@gmail.com>
2020-01-23 11:44:24 +0200

    Added status constants and upgraded way of loading lanfuages into dict

--- constants.py
+++ constants.py
@@ -1,6 +1,24 @@
 import json
+from pprint import pprint
+LANG_CHOOSE_STATUS = "LANG_CHOOSE"
+NORMAL_STATUS = "NORMAL"
+CATALOG_STATUS = "CATALOG"
+IN_GAME = "IN_GAME"
+GAME_REQUEST = "GAMEREQUEST"
+LANG = {}
 
-PHRASES = {}
-with open('data/phrases.json', 'r') as f:
-    PHRASES = json.load(f)
+def instansiate_language(lang):
+    with open("data/EN.json") as f:
+        lang['en'] = json.load(f)
+
+    with open("data/RU.json") as f:
+        lang['ru'] = json.load(f)
+
+    with open("data/UA.json") as f:
+        lang['ua'] = json.load(f)
+
+instansiate_language(LANG)
+
+if __name__ == '__main__':
+    pprint(LANG)
 
